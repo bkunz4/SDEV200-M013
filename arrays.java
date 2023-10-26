e5235459978d72e0be4cd95f37313ea07ed732de
bkunz4 <23880812-bkunz4@users.noreply.replit.com>
2023-10-26 19:44:26 +0000

    final

--- arrays.java
+++ arrays.java
@@ -4,8 +4,8 @@ public class arrays {
 public static boolean equals(int[][] m1, int[][] m2) {
 /*Returns true if m1 and m2 are identical*/
 if((m1.length != m2.length) || (m1[0].length != m2[0].length)) return false;
-for(int i=0;i<m1.length;i++) {
-for(int j=0;j<m1[i].length;j++) {
+for(int i=0; i<m1.length; i++) {
+for(int j=0; j<m1[i].length; j++) {
 if(m1[i][j] != m2[i][j]) return false;
 }
 }
@@ -20,16 +20,16 @@ int m2[][] = new int[3][3];
 
 /*Asks user to enter the first 3x3 array*/
 System.out.println("Enter m1:");
-for(int i=0;i<m1.length;i++) {
-for(int j=0;j<m1[0].length;j++) {
+for(int i=0; i<m1.length; i++) {
+for(int j=0; j<m1[0].length; j++) {
 m1[i][j] = in.nextInt();
 }
 }
 
 /*Asks user to enter the second 3x3 array*/
 System.out.println("Enter m2:");
-for(int i=0;i<m1.length;i++) {
-for(int j=0;j<m1[0].length;j++) {
+for(int i=0; i<m1.length; i++) {
+for(int j=0; j<m1[0].length; j++) {
 m2[i][j] = in.nextInt();
 }
 }
